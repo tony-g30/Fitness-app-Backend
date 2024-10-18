6259a3ab3e7fd25ad5c50d2f57a0ca17b981350b
nahashon <nashonmwendwa0@gmail.com>
2024-10-18 14:41:35 +0300

    modifies config.py

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 # config.py
 import os
 
+#main config
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'default-secret-key'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///site.db'
