87d6fe7a91a59b296b60b48c68722457135cf2f7
nahashon <nashonmwendwa0@gmail.com>
2024-10-18 14:23:23 +0300

    Implement user registration and login with JWT token generation

---
+++ auth.py
@@ -0,0 +1,30 @@
+from flask import request, jsonify
+from flask_jwt_extended import create_access_token
+from models import db, User
+
+def register():
+    data = request.get_json()
+    username = data.get('username')
+    password = data.get('password')
+
+    if User.query.filter_by(username=username).first():
+        return jsonify({'msg': 'User already exists'}), 400
+
+    new_user = User(username=username)
+    new_user.set_password(password)
+    db.session.add(new_user)
+    db.session.commit()
+
+    return jsonify({'msg': 'User created successfully'}), 201
+
+def login():
+    data = request.get_json()
+    username = data.get('username')
+    password = data.get('password')
+
+    user = User.query.filter_by(username=username).first()
+    if user and user.check_password(password):
+        access_token = create_access_token(identity=user.id)
+        return jsonify(access_token=access_token), 200
+
+    return jsonify({'msg': 'Invalid credentials'}), 401
